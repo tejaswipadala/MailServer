e0f723eb5d11424cf0b83376ff89b6518a7d4f01
tejaswipadala <61086923+tejaswipadala@users.noreply.github.com>
2022-01-19 19:34:06 +0530

    Background Color Change
    Changed the background color to white.

--- aboutus.php
+++ aboutus.php
@@ -1,4 +1,4 @@
-<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<h2 style="background:#white;padding:5px" align="left">About us</h2>
 <p> We are Students. SOFTWARE TESTING J COMPONENT <BR>
     19BIT0310 Tejaswi Padala <BR>
     19BIT0287 Parineetha Parihar
